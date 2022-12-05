a95cc7fc64c5ce476bc783ab97c2d6b863e9dc4b
Amr Atif <62966382+amr-atif@users.noreply.github.com>
2022-12-05 21:24:59 +0200

    user-authentication

--- django_projct/settings.py
+++ django_projct/settings.py
@@ -44,6 +44,12 @@ INSTALLED_APPS = [
     #3rd party apps 
     'rest_framework',
     'corsheaders',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration'
 
 
 ]
@@ -52,6 +58,10 @@ REST_FRAMEWORK = { # new
 "DEFAULT_PERMISSION_CLASSES": [
 "rest_framework.permissions.IsAuthenticated",
 ],
+"DEFAULT_AUTHENTICATION_CLASSES":[
+    "rest_framework.authentication.SessionAuthentication",
+    "rest_framework.authentication.TokenAuthentication",
+]
 }
 
 MIDDLEWARE = [
@@ -83,10 +93,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request'
             ],
         },
     },
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID=1
+
 
 WSGI_APPLICATION = 'django_projct.wsgi.application'
 
